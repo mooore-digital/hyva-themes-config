4fd3cac69e9cb10e309a2b442ebf89fb2f67c2dc
GrimLink <sean.grimlink@gmail.com>
2023-07-26 10:56:07 +0200

    ADD: cssprops as sub package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- `@fylgja/tailwindcss-plugin-cssprops` as sub package for easier importing,
+  also plays really well with our own `createTailwindTokens()` function
 
 ## [1.1.0] - 2022-01-11
 ### Added
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+const cssprops = require("@fylgja/tailwindcss-plugin-cssprops");
+const { createTailwindTokens } = require("./src/twTokens.cjs");
 const { rootPath, breakpoints } = require("./src/common.cjs");
 const {
     proseCleanup,
     twTypoMargin,
     proseDefaults,
 } = require("./src/typography.cjs");
-const { createTailwindTokens } = require("./src/twTokens.cjs");
 
 module.exports = {
-    rootPath,
     breakpoints,
-    proseCleanup,
-    twTypoMargin,
-    proseDefaults,
     createTailwindTokens,
+    cssprops,
+    proseCleanup,
+    proseDefaults,
+    rootPath,
+    twTypoMargin,
 };
